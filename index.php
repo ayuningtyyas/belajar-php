b6a4d9f7e8dddd2df745f7dc55759b6f695cbc4e
catur wahyu sari ayuningtyas <ayuningtyyas2205@gmail.com>
2022-11-24 17:05:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Hallo!!<br>";
+
+$nama = "Tyas";
+$umur = 21;
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur Tahun.";                                
+
+$namaKakak = "Iis";
+$umurKakak = 31;
+echo "<br>Nama kakak saya $namaKakak, usia kakak saya $umurKakak Tahun.";
+
+$selisih = $umur-$umurKakak;
+echo "<br>Selisih usia saya dan kakak saya adalah $selisih Tahun.";
+?>
